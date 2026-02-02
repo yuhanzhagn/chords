771587e9cc01b2f55af6fcbba45818eda5f94ece
yuhanzhagn <yzhang@gmail.com>
2026-02-03 00:16:18 +0900

    Updates readme and docker-compose

--- backend/internal/app/db.go
+++ backend/internal/app/db.go
@@ -1,39 +1,38 @@
 package app
 
 import (
-    "log"
-    "gorm.io/driver/sqlite"
-    "gorm.io/gorm"
 	"backend/internal/model"
+	"log"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
 )
 
 func InitDB(db *gorm.DB) error {
-    return db.AutoMigrate(
-        &model.User{},
-        &model.ChatRoom{},
-        &model.Message{},
+	return db.AutoMigrate(
+		&model.User{},
+		&model.ChatRoom{},
+		&model.Message{},
 		&model.UserSession{},
 		&model.UserChatRoom{},
-    )
+	)
 }
 
+func ConnectDB(cfg *Config) (*gorm.DB, error) {
+	/* cfg, err := config.LoadConfig("config.yaml")
+	   if err != nil {
+	       log.Fatal("Failed to load config:", err)
+	   } */
 
-func ConnectDB(cfg *Config)(*gorm.DB, error){
-    /* cfg, err := config.LoadConfig("config.yaml")
-    if err != nil {
-        log.Fatal("Failed to load config:", err)
-    } */
+	// Only handling sqlite for now
+	if cfg.Database.Dialect != "sqlite" {
+		log.Fatal("Unsupported DB dialect:", cfg.Database.Dialect)
+	}
+	db, err := gorm.Open(sqlite.Open(cfg.Database.DSN), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
 
-    // Only handling sqlite for now
-    if cfg.Database.Dialect != "sqlite" {
-        log.Fatal("Unsupported DB dialect:", cfg.Database.Dialect)
-    }
-   	db, err := gorm.Open(sqlite.Open(cfg.Database.DSN), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect to database:", err)
-    }
-
-    log.Println("Database connected.")
+	log.Println("Database connected.")
 	return db, err
 }
-
--- backend/test/controller_service_test/auth_cs_test.go
+++ backend/test/controller_service_test/auth_cs_test.go
@@ -1,62 +1,65 @@
 package controller_service_test
 
-import(
-    "testing"
-    "time"
+import (
 	"encoding/json"
-//	"errors"
+	"testing"
+	"time"
+
+	//	"errors"
 	"bytes"
 
 	"github.com/gin-contrib/cors"
-//    "github.com/stretchr/testify/assert"
+	//    "github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-//	"github.com/stretchr/testify/mock"
-	"github.com/gin-gonic/gin"
+	//	"github.com/stretchr/testify/mock"
 	"net/http"
 	"net/http/httptest"
-    "gorm.io/driver/sqlite"
-    "gorm.io/gorm"
 
-    "backend/internal/model"
-    "backend/internal/repo"
-    "backend/internal/service"
-	"backend/internal/controller"
+	"github.com/gin-gonic/gin"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+
 	"backend/internal/cache"
-//	"backend/internal/middleware/jwtauth"
+	"backend/internal/controller"
+	"backend/internal/model"
+	"backend/internal/repo"
+	"backend/internal/service"
+
+	//	"backend/internal/middleware/jwtauth"
+	"backend/internal/logrus"
 	"backend/internal/middleware/loadshedding"
 	"backend/internal/middleware/logger"
-	"backend/internal/logrus"
-//	"backend/utils"
+	//	"backend/utils"
 )
 
 func setupTestDB() *gorm.DB {
-    db, _ := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
-    //assert.NoError(t, err, "failed to connect database")
+	db, _ := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	//assert.NoError(t, err, "failed to connect database")
 
-    // Migrate schema
-    _ = db.AutoMigrate(&model.User{}, &model.UserSession{}, &model.Message{}, &model.ChatRoom{}, &model.UserChatRoom{})
-    //assert.NoError(t, err, "failed to migrate database")
+	// Migrate schema
+	_ = db.AutoMigrate(&model.User{}, &model.UserSession{}, &model.Message{}, &model.ChatRoom{}, &model.UserChatRoom{})
+	//assert.NoError(t, err, "failed to migrate database")
 
-    return db
+	return db
 }
 
-func setupBasicMiddleware() *gin.Engine{
+func setupBasicMiddleware() *gin.Engine {
 	r := gin.New()
 	// init middlewares
 	log := logrus.InitLogrus()
 	r.Use(logger.LogrusLogger(log))
 	r.Use(gin.Recovery())
 	r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"*"}, // or "*" for all origins
-        AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-        AllowCredentials: true,
-        MaxAge:           12 * time.Hour,
-    }))
+		AllowOrigins:     []string{"*"}, // or "*" for all origins
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	return r
 }
 
-func setupAuthRoute(r *gin.Engine){
+func setupAuthRoute(r *gin.Engine) {
 	db := setupTestDB()
 	repos := repo.NewRepoContainer(db)
 	typedCache := cache.NewTypedCache[service.BlockEntry](10*time.Minute, 15*time.Minute)
@@ -67,20 +70,20 @@ func setupAuthRoute(r *gin.Engine){
 
 	authRoutes := r.Group("/api/auth")
 	authRoutes.Use(loadsheddingFunc)
-    {
-        authRoutes.POST("/register", authController.Register)
-        authRoutes.POST("/login", authController.Login)
+	{
+		authRoutes.POST("/register", authController.Register)
+		authRoutes.POST("/login", authController.Login)
 		authRoutes.POST("/logout", authController.Logout)
-    } 
+	}
 }
 
-func TestRegister(t *testing.T){
+func TestRegister(t *testing.T) {
 	r := setupBasicMiddleware()
 	setupAuthRoute(r)
 
 	body := map[string]string{
 		"username": "john",
-		"email":"crazyjohn@test.com",
+		"email":    "crazyjohn@test.com",
 		"password": "correct",
 	}
 	jsonBody, _ := json.Marshal(body)
@@ -92,7 +95,7 @@ func TestRegister(t *testing.T){
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 
-    require.Equal(t, http.StatusOK, w.Code)
+	require.Equal(t, http.StatusOK, w.Code)
 
 	//test login
 	body = map[string]string{
@@ -107,17 +110,17 @@ func TestRegister(t *testing.T){
 	w = httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	require.Equal(t, http.StatusOK, w.Code)
-    require.Contains(t, w.Body.String(), `"token"`)
+	require.Contains(t, w.Body.String(), `"token"`)
 
 	// test logout
 	var m map[string]interface{}
 	if err := json.Unmarshal([]byte(w.Body.String()), &m); err != nil {
-		require.NoError(t,err)
-	} 
+		require.NoError(t, err)
+	}
 	token := m["token"].(string)
 	req = httptest.NewRequest(http.MethodPost, "/api/auth/logout", bytes.NewBuffer(nil))
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer " + token)
+	req.Header.Set("Authorization", "Bearer "+token)
 	w = httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	require.Equal(t, http.StatusOK, w.Code)
